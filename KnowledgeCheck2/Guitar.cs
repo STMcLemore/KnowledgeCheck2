27e80be6866f8961db5327ffb8fa1dc9c56a1e57
Stephen <stmclemore@hotmail.com>
2024-09-14 21:18:22 -0400

    I did it!

---
+++ KnowledgeCheck2/Guitar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck2
+{
+    public class Guitar : MusicalInstrument
+    {
+        public string GuitarColor { get; set; }
+
+        public override string ToString()
+        {
+            return ($"Brand: {Brand}, Color: {GuitarColor}");
+        }
+    }
+}
---
+++ KnowledgeCheck2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using KnowledgeCheck2;
+
+Console.WriteLine("How many guitars do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+
+
+var recordList = new List<Guitar>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+    // In this loop, populate the object's properties using Console.ReadLine()
+    var guitar = new Guitar();
+
+    Console.WriteLine("Enter the brand of guitar ");
+    guitar.Brand = Console.ReadLine();
+    Console.WriteLine();
+
+    Console.WriteLine("What color is the guitar?");
+    guitar.GuitarColor = Console.ReadLine();
+    Console.WriteLine();
+
+    recordList.Add(guitar);
+}
+
+// Print out the list of records using Console.WriteLine()
+foreach(var guitar in recordList)
+{
+    Console.WriteLine(guitar.ToString());
+}
